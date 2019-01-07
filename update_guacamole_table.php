84564f551efb431ae15a5bf91b2b78b80d275489
ochamber <ochamber@users.noreply.github.com>
2019-01-07 14:55:51 +0100

    Update update_guacamole_table.php

--- update_guacamole_table.php
+++ update_guacamole_table.php
@@ -6,12 +6,12 @@ $connect = dbConnection();
 
 $username="username"; //Your credentials goes here
 $password="password";
-$base_url='https://myguacamoleserver.mydomain.com/guacamole';
-$service_url = $base_url . '/api/tokens';
+$base_url='https://myguacamoleserver.mydomain.com/guacamole/';
+$service_url = $base_url . 'api/tokens';
 $curl = curl_init($service_url);
 curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 curl_setopt($curl, CURLOPT_USERNAME, $username);
-curl_setopt($curl, CURLOPT_PASSWORD, $password); 
+curl_setopt($curl, CURLOPT_PASSWORD, $password);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_POST, true);
 //curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
@@ -19,27 +19,21 @@ curl_setopt($curl, CURLOPT_POST, true);
 $curl_response = curl_exec($curl);
 $response = json_decode($curl_response);
 curl_close($curl);
-
 //echo "authToken: ";
 //echo $response->authToken;
-
-$complete_url=$base_url.'/api/session/data/mysql/users?token='.$response->authToken;
-
+$complete_url=$base_url.'api/session/data/mysql/users?token='.$response->authToken;
 $ch_guacamole = curl_init();
 curl_setopt($ch_guacamole, CURLOPT_RETURNTRANSFER, 1);
 //curl_setopt($ch_guacamole, CURLOPT_HTTPHEADER, array('PRIVATE-TOKEN:'));
 curl_setopt($ch_guacamole, CURLOPT_URL, $complete_url);
 $res = curl_exec($ch_guacamole);
 curl_close($ch_guacamole);
-
 $data = json_decode($res);
-
 $searched_attribute="guac-email-address"; //2nd level of json
 $id=1;
 foreach ($data as $row){
 	$json = $row->attributes;
 	$value=$json->$searched_attribute;
-
 $sql = <<<TAG
 INSERT INTO guacamole_users (id, email) VALUES ('$id', '$value')
 TAG;
